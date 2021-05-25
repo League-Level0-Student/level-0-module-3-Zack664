7ef8a0acffa3cfc0bff66c8393567d9e7930ed99
root <root@0bbc87aacb31>
2021-05-25 23:56:28 +0000

    HLG ip!

--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -13,12 +13,29 @@ public class CrazyCatLady {
 		// 2. Convert their answer into an int
 	int number = Integer.parseInt(HMC);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-	if HMC.ch(eqaulsIgnoreCase.>3)
+	if (number>3) {
+		JOptionPane.showMessageDialog(null, "You are a crazy cat Lady!");
+		
+		
+		
+	}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 		//    Hint: You can use the && operator 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
+	if (number<3) {
+		JOptionPane.showMessageDialog(null, "https://www.youtube.com/watch?v=tpiyEe_CqB4");
 		
 		
+		
+	}
+	if (number<0) {
+		JOptionPane.showMessageDialog(null, "https://www.youtube.com/watch?v=k85mRPqvMbE\n" + 
+				"\n" + 
+				"");
+		
+		
+		
+	}
 		
 				
 		
@@ -302,6 +319,6 @@ public class CrazyCatLady {
 			java.awt.Desktop.getDesktop().browse(uri);
 		} catch (Exception e) {
 			e.printStackTrace();
-		
+		}
 		}
 	}
